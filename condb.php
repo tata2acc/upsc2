43e3e7077e1969abeef8655997b8eb48b472f183
ekkachai1248 <tata2@acc.msu.ac.th>
2024-03-17 12:00:24 +0700

    a

--- condb.php
+++ condb.php
@@ -1,11 +1,10 @@
 <meta charset="utf-8">
 <?php
 	// $pwd="Q2ijj48vgn";
-
     $host="localhost";
 	$user="root";
 	$pwd="7ci838a!nK";
-	//$pwd="";
+	// $pwd="";
 	$db="ablerex_ups";
 
 	$conn = mysqli_connect($host, $user, $pwd)or die("เชื่อมต่อฐานข้อมูลไม่ได้");
